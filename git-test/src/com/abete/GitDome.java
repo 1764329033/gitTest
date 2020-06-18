ce1faabadec2c68f9066f139ecf11bd4a8747c05
zhiwu <1764329033@qq.com>
2020-06-18 11:12:42 +0800

    构建了一个项目

---
+++ git-test/src/com/abete/GitDome.java
@@ -0,0 +1,11 @@
+package com.abete;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GitDome {
+    public static void main(String[] args) {
+        System.out.println("项目经理构建了项目，并上传到github上");
+
+    }
+}
