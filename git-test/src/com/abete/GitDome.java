06a316a4cd65cc15417d41f83d2a6740e7e86be1
zhiwu <1764329033@qq.com>
2020-06-18 11:47:26 +0800

    小王增加了功能

--- git-test/src/com/abete/GitDome.java
+++ git-test/src/com/abete/GitDome.java
@@ -6,6 +6,6 @@ import java.util.List;
 public class GitDome {
     public static void main(String[] args) {
         System.out.println("项目经理构建了项目，并上传到github上");
-
+        System.out.println("小王增加了一些功能");
     }
 }
