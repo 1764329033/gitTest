b5aa200320bb05a4530015952ab4aa6a6b04f7e9
zhiwu <1764329033@qq.com>
2020-06-18 14:10:50 +0800

    在分支上增加了功能

--- git-test/src/com/abete/GitDome.java
+++ git-test/src/com/abete/GitDome.java
@@ -8,4 +8,9 @@ public class GitDome {
         System.out.println("项目经理构建了项目，并上传到github上");
         System.out.println("小王增加了一些功能");
     }
+    public void insertUser(){
+
+        System.out.println("在分支上增加的功能");
+
+    }
 }
